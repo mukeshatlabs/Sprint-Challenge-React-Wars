15f914151e6a45c19c7ce5edbb008e1198fd107b
Jain <mukjain@acbc32a00fc7.ant.amazon.com>
2020-04-19 20:22:14 -0700

    finished mvp

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import { Container, Row } from "reactstrap";
+
+const Character = () => {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then(response => {
+        setCharacters(response.data);
+        console.log("characters: ", response.data.results)
+      })
+      .catch(error => console.log("Error!", error));
+  }, []);
+  if (!characters.results) return <h3>Loading...</h3>;
+  return (
+    <div>
+        <Container>
+            <Row>
+                {characters.results.map(character=>{
+                    return <CharacterCard character={character} key={character.id} />;
+                })}
+            </Row>
+        </Container>
+    </div>
+  );
+}
+
+export default Character;
